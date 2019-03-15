e6ed2a16d7cffcf7467eace7147b065e1ceb0ea1
domybestformyself <45992524+domybestformyself@users.noreply.github.com>
2019-03-16 03:05:56 +0800

    Update HelloGithub.java

--- idea/Test/src/HelloGithub.java
+++ idea/Test/src/HelloGithub.java
@@ -2,5 +2,6 @@ public class HelloGithub {
     public static void main(String[] args) {
         System.out.println("Hello Github");
         System.out.println("Hello java");
+         System.out.println("Hello java");
     }
 }
